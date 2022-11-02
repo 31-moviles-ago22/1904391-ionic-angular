73aef414820456a51254c44f5cc6a673767f800e
Gus1904391 <111162670+Gus1904391@users.noreply.github.com>
2022-11-01 22:44:31 -0600

    articulo-detalle

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from './app-routing.module';
+import { ArticulosComponent } from './articulos/articulos.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ArticuloDetalleComponent } from './articulo-detalle/articulo-detalle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    ArticulosComponent,
+    InicioComponent,
+    ArticuloDetalleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ proyecto-angular/src/app/articulos/articulos.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-articulos',
+  templateUrl: './articulos.component.html',
+  styleUrls: ['./articulos.component.css']
+})
+export class ArticulosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  articulos: any = [
+    {
+      id: 1,
+      nombre: "Gorra", 
+      imagen: 'assets/imagenes/gorra.jpg',
+      precio: 299
+    },
+    {
+      id: 2,
+      nombre: "Taza", 
+      imagen: 'assets/imagenes/taza.jpg',
+      precio: 199
+    },
+    {
+      id: 3,
+      nombre: "Camiseta", 
+      imagen: 'assets/imagenes/playera.jpg',
+      precio: 199
+    },
+    {
+      id: 4,
+      nombre: "Bolsa", 
+      imagen: 'assets/imagenes/bolsa.jpg',
+      precio: 99
+    }
+  ];
+
+}
