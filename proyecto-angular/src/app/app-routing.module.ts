852c3490659ee58b73f83f1fcde26631339204b6
Gus1904391 <111162670+Gus1904391@users.noreply.github.com>
2022-11-01 22:51:29 -0600

    Edicion Articulo detalle

--- proyecto-angular/src/app/app-routing.module.ts
+++ proyecto-angular/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
+import { Router, RouterModule, Routes} from '@angular/router';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ArticulosComponent } from './articulos/articulos.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ArticuloDetalleComponent } from './articulo-detalle/articulo-detalle.component';
 
-const routes: Routes = [];
+const rutas : Routes = [
+    {
+        path: 'articulos', 
+        component: ArticulosComponent
+    },
+    {
+        path: 'inicio', 
+        component: InicioComponent
+    },
+    {
+        path: 'detalle/:id',
+        component: ArticuloDetalleComponent 
+    },
+    {
+        path: '',
+        redirectTo: '/inicio', 
+        pathMatch: 'full'
+    }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [ RouterModule.forRoot(rutas)], 
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
